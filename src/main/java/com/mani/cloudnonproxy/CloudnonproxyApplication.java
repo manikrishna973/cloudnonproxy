36a45de7aa367279dac76a1244ebde1135772d0e
mani <manikrishna973@gmail.com>
2020-11-03 10:52:50 +0530

    initial commit

---
+++ src/main/java/com/mani/cloudnonproxy/CloudnonproxyApplication.java
@@ -0,0 +1,13 @@
+package com.mani.cloudnonproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudnonproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudnonproxyApplication.class, args);
+	}
+
+}
